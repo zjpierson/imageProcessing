3d07b555c63f7a7d735b89c38f006eb69f546fd7
zjpierson <zachary.pierson@mines.sdsmt.edu>
2015-01-26 10:08:22 -0700

    Revert "changed file structure"
    This reverts commit 0b20db5660d71bb749adb3543f05824272c2424a.

---
+++ project1/program1.h
@@ -0,0 +1,45 @@
+/*
+               ***** example2.h *****
+
+Demo program to illustrate qtImageLib.
+Example 2 shows how to add menu items to the default image viewer.
+
+Author: John M. Weiss, Ph.D.
+Written Spring 2015 for SDSM&T CSC442/542 DIP course.
+
+Modifications:
+*/
+
+// next two lines are typical
+#pragma once
+#include <qtimagelib.h>
+
+// class declaration
+class MyApp : public QObject
+{
+    Q_OBJECT;	// QOBJECT macro must appear at start of class
+
+    // all menu items must be prototyped in the "public slots" section
+    // note the method prototype and naming conventions:
+    // bool Menu_Menubar_Menuitem( Image & );
+
+  public slots:
+
+    bool Menu_Palette_Grayscale( Image & );
+    bool Menu_Palette_Negate( Image & );
+    bool Menu_Palette_Threshold( Image & );
+    bool Menu_Palette_Posterize( Image & );
+    bool Menu_Palette_Brightness( Image & );
+    bool Menu_Palette_Contrast( Image & );
+    bool Menu_Palette_Gamma( Image & );
+    bool Menu_Palette_Log( Image & );
+   
+    bool Menu_Histogram_Display( Image & );
+    bool Menu_Histogram_AutoStretch( Image & );
+    bool Menu_Histogram_ModStretch( Image & );
+    bool Menu_Histogram_Equalize( Image & );
+    bool Menu_Histogram_ClipEqualize( Image & );
+
+    bool Menu_Color_DiscretePseudocolor( Image & );
+    bool Menu_Color_ContinuousPseudocolor( Image & );
+};
---
+++ project1/program1.pro
@@ -0,0 +1,3 @@
+HEADERS += program1.h
+SOURCES += program1.cpp
+CONFIG += qtimagelib
