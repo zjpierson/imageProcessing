8f3ff6cc4102154b2fb2764492ad326734b18bfb
Kayhan Karatekeli <kayhan.karatekeli@gmail.com>
2015-02-25 15:41:10 -0700

    added comments and headers for comments

--- pa2/noise_menu.cpp
+++ pa2/noise_menu.cpp
@@ -1,6 +1,25 @@
+/************************************************************************
+* Program: Neighborhood Processing - Assignment 2 
+* Author: Kayhan Karatekeli & Zachary Pierson
+* Class: CSC 442 - Digital Image Processing
+* Instructor: Dr. John Weiss
+* Date: 2/26/15
+* Description: This program is designed to apply spacial-domain filters
+*              and neighborhood operators to an image.
+*
+* File: This file contains the noise operators supplied in the noise menu.
+************************************************************************/
+
 #include "pa2.h"
 
-
+/************************************************************************
+Function:     Gaussian
+Description:  This function adds gaussian noise to an image using the 
+              built in function in QT.
+Parameters:   param[in]  image - The image being converted.
+              param[out] true  - Updates the image upon completion.
+              param[out] false - Exits function if image is NULL.
+************************************************************************/
 bool MyApp::Menu_Noise_Gaussian( Image &image )
 {
     double sd = 8.0;
@@ -10,6 +29,14 @@ bool MyApp::Menu_Noise_Gaussian( Image &image )
     return true;
 }
 
+/************************************************************************
+Function:     Impulse
+Description:  This function adds impulse noise to an image using the 
+              built in function in QT.
+Parameters:   param[in]  image - The image being converted.
+              param[out] true  - Updates the image upon completion.
+              param[out] false - Exits function if image is NULL.
+************************************************************************/
 bool MyApp::Menu_Noise_Impulse( Image &image )
 {
     int p = 64;
